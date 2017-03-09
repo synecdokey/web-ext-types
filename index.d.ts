984d3b568a9de0e35159888479ab1450ab597109
Michael Zapata <michael.zapata@scality.com>
2017-03-09 23:05:32 +0100

    chore(license): add GPLv3
    No need for a more 'permissive' license as plugins don't really need a
    different license than this one

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,19 @@
+//    web-ext-types are TypeScript type definitions for WebExtensions
+//    Copyright (C) 2017  Michael Zapata
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 interface Listener<T> {
     addListener: (callback: (arg: T) => void) => void;
     removeListener: (listener: Listener<T>) => void;
