21221c1dabcc54c657991f6f9aba0cdf473476ad
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-10-13 21:01:32 +0200

    storage.get returns typed object when object is passed in parameter

--- global/index.d.ts
+++ global/index.d.ts
@@ -834,8 +834,13 @@ declare namespace browser.storage {
         [key: string]: any
     }
 
+    interface Get {
+        (keys: string|string[]|null): Promise<StorageResults>;
+        <T>(keys: T): Promise<T>;
+    }
+
     type StorageArea = {
-        get: (keys: string|string[]|object|null) => Promise<StorageResults>,
+        get: Get,
         // unsupported: getBytesInUse: (keys: string|string[]|null) => Promise<number>,
         set: (keys: object) => Promise<void>,
         remove: (keys: string|string[]) => Promise<void>,
